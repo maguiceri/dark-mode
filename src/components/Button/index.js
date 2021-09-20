0ba9064fd4f51fdebce53f3bcfb81c0e97e3844c
magui <magui@Maguis-MacBook-Pro.local>
2021-09-19 21:46:34 -0300

    styles

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -5,7 +5,7 @@ import ThemeContext from "../../contexts/ThemeContext";
 function Button(props) {
     const theme = useContext(ThemeContext);
     return(
-        <button className={theme} onClick={() => props.handleTheme()}>Soy un boton</button>
+        <button className={`${theme} button`} onClick={() => props.handleTheme()}>Soy un boton</button>
     )
 }
 
